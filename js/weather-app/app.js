db58e21e008a0fbdbe77114e618828fd08c2a79e
Merrill Lines <linesmerrill@gmail.com>
2016-09-09 16:09:09 -0700

    chore (index.html) changed some text size and position

--- js/weather-app/app.js
+++ js/weather-app/app.js
@@ -18,6 +18,20 @@ var app = angular.module('WeatherApp',[])
         };
     });
 
+		// filter to convert direction degree to cardinal direction
+	// .filter('customDegreeToCardinalFilter', function () {
+	// 		return function (deg) {
+	// 			if(deg > 337 || deg < 22) {return 'N';}
+	// 			else if(deg > 22 && deg < 67) {return 'NE';}
+	// 			else if(deg > 67 && deg < 112) {return 'E';}
+	// 			else if(deg > 112 && deg < 157) {return 'SE';}
+	// 			else if(deg > 157 && deg < 202) {return 'S';}
+	// 			else if(deg > 202 && deg < 247) {return 'SW';}
+	// 			else if(deg > 247 && deg < 292) {return 'W';}
+	// 			else if(deg > 292 && deg < 337) {return 'NW';}
+	// 		};
+	// 	});
+
 /**
  * I prepend zeros to a number
  * @param integer n
